392bb574f0b29ae07964da98fcf377bb4314b31c
sghctoma <sghctoma@gmail.com>
2016-04-30 22:07:27 +0200

    Added FreeBSD ifconfig support to PacketFu::Utils.ifconfig.

--- lib/bettercap/monkey/packetfu/utils.rb
+++ lib/bettercap/monkey/packetfu/utils.rb
@@ -49,8 +49,10 @@ module PacketFu
           ret = linux_ifconfig iface, data
         when /darwin/i
           ret = darwin_ifconfig iface, data
-	      when /openbsd/i
-	        ret = openbsd_ifconfig iface, data
+        when /openbsd/i
+          ret = openbsd_ifconfig iface, data
+        when /freebsd/i
+          ret = freebsd_ifconfig iface, data
         end
       elsif BetterCap::Shell.available?('ip')
         BetterCap::Logger.debug "Using iproute2"
@@ -157,6 +159,37 @@ module PacketFu
             ret[:eth_saddr] = $1
             ret[:eth_src] = EthHeader.mac2str(ret[:eth_saddr])
           when /inet[\s]*([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(.*Mask[\s]+(0x[a-f0-9]+))?/i
+            imask = 0
+            if $3
+              imask = $3.to_i(16).to_s(2).count("1")
+            end
+
+            ret[:ip_saddr] = $1
+            ret[:ip_src] = [IPAddr.new($1).to_i].pack("N")
+            ret[:ip4_obj] = IPAddr.new($1)
+            ret[:ip4_obj] = ret[:ip4_obj].mask(imask) if imask
+          when /inet6[\s]*([0-9a-fA-F:\x2f]+)/
+            ret[:ip6_saddr] = $1
+            ret[:ip6_obj] = IPAddr.new($1)
+        end
+      end
+
+      ret
+    end
+
+    def self.freebsd_ifconfig(iface='eth0',ifconfig_data)
+      BetterCap::Logger.debug "FreeBSD ifconfig #{iface}:\n#{ifconfig_data}"
+
+      ret = {}
+      real_iface = ifconfig_data.first
+      ret[:iface] = real_iface.split(':')[0]
+
+      ifconfig_data.each do |s|
+        case s
+          when /ether[\s]([0-9a-fA-F:]{17})/i
+            ret[:eth_saddr] = $1
+            ret[:eth_src] = EthHeader.mac2str(ret[:eth_saddr])
+          when /inet[\s]*([0-9]+\.[0-9]+\.[0-9]+\.[0-9]+)(.*netmask[\s]+(0x[a-f0-9]+))?/i
             imask = 0
             if $3
               imask = $3.to_i(16).to_s(2).count("1")
